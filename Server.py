bb7397f7b58510d7d175c4616926ac9a2c9cd583
Yousif <Yousif.alshurooqi@gmail.com>
2024-05-26 14:57:56 +0300

    Creating a new function called retrieve news

--- Server.py
+++ Server.py
@@ -19,4 +19,17 @@ def startServer():
         client_handler = threading.Thread(target=manageUserRequest, args=(client_socket, addr))
         client_handler.start()
 
+# Function to retrieve news data from the NewsAPI
+def retrieveNews (endpoint, params):
+    # Add the API key to the parameters
+    params['apiKey'] = The_ApiKey
+    # Send a GET request to the NewsAPI
+    response = requests.get(TheURL + endpoint, params=params)
+    # Check if the request was successful
+    if response.status_code == 200:
+        # Return the JSON data
+        return response.json()
+    else:
+        # Return an error message
+        return {'status': 'error', 'message': 'Unable to fetch data'}
 
