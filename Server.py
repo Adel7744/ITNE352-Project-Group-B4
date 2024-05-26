5728116612a1b5f159030bfc8f320e373f480f88
Yousif <Yousif.alshurooqi@gmail.com>
2024-05-26 13:14:58 +0300

    Importing necessary modules and Defining API key and URL for NewsAPI and Setting the host and port for the socket server

--- Server.py
+++ Server.py
@@ -1,1 +1,9 @@
-print("22225")
+import socket
+import json
+import threading
+import requests
+
+The_ApiKey = 'c7f93d27e641492e9f7ee1f87e5ea186'
+TheURL = 'https://newsapi.org/v2/'
+HOST = '127.0.0.1'
+PORT = 5555
